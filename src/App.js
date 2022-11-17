b9509902b511052c351c17d25a7d7cbed4f2c8cd
Chetonix <chetonix@gmail.com>
2022-11-17 15:59:27 +0530

    replace class with className and for with htmlFor

--- src/App.js
+++ src/App.js
@@ -12,23 +12,23 @@ function App() {
 
   return (
     <div id="container">
-      <div class="form-wrap">
+      <div className="form-wrap">
         <h1>Sign Up</h1>
         <p>It's free and only takes a minute</p>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <div class="form-group">
-            <label for="first-name">First Name</label>
+          <div className="form-group">
+            <label htmlFor="first-name">First Name</label>
             <input type="text" name="firstName" id="first-name" {...register("firstName" , { required: true, maxLength: 10 })}/>
             
           </div>
           {errors.firstName && <p style={{color: "red"}}>Please check the First Name</p>}
-          <div class="form-group">
-            <label for="last-name">Last Name</label>
+          <div className="form-group">
+            <label htmlFor="last-name">Last Name</label>
             <input type="text" name="lastName" id="last-name" {...register("lastName" , { required: true, maxLength: 10 })}/>
           </div>
           {errors.lastName && <p style={{color: "red"}}>Please check the Last Name</p>}
-          <div class="form-group">
-            <label for="email">Email</label>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
             <input type="email" name="email" id="email" {...register("email",
                             {
                                 required: true,
@@ -37,8 +37,8 @@ function App() {
           </div>
           {errors.email && <p style={{color: "red"}}>Please check the Email</p>}
 
-          <div class="form-group">
-            <label for="phone">Mobile No.</label>
+          <div className="form-group">
+            <label htmlFor="phone">Mobile No.</label>
             <input type="phone" name="phone" id="phone" {...register("phone", {
                             required: true,
                             pattern: /^[789]\d{9}$/
@@ -46,8 +46,8 @@ function App() {
           </div>
           {errors.phone && <p style={{color: "red"}}>The Phone no. must be 10 digit long.</p>}
 
-          <div class="form-group">
-            <label for="password">Password</label>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
             <input type="password" name="password" id="password" {...register("password", {
                             required: true,
                             pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15}$/
@@ -55,8 +55,8 @@ function App() {
           </div>
           {errors.password && <p style={{color: "red"}}>Password must contain one capital letter, one small letter, one number and must be 6 - 15 chars long.</p>}
 
-          <div class="form-group">
-            <label for="password2">Confirm Password</label>
+          <div className="form-group">
+            <label htmlFor="password2">Confirm Password</label>
             <input type="password" name="password2" id="password2"  {...register("confirmPassword", {
                             required: true,
                             validate: (val: string) => {
@@ -68,8 +68,8 @@ function App() {
           </div>
           {errors.confirmPassword && errors.confirmPassword.type === "validate" && <p  style={{color: "red"}}>Passwords do not match</p>}
 
-          <button type="submit" class="btn">Sign Up</button>
-          <p class="bottom-text">
+          <button type="submit" className="btn">Sign Up</button>
+          <p className="bottom-text">
             By clicking the Sign Up button, you agree to our
             <a href="#">Terms & Conditions</a> and
             <a href="#">Privacy Policy</a>
